ae73f31561014d92e8a8f3d997e45938b0ffe4d7
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2025-01-22 13:19:24 -0600

    updated comments in rds section

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -180,7 +180,7 @@ resource "aws_iam_user_policy_attachment" "ec2" {
 
 # Data is the keyword to define something we can use in a resource
 # Allows to manage RDS resources, intead of specifying the resources we use *, which means all resources
-# For this example we will use it, because we still don't have the RDS instance and we don't know which one well use
+# For this example we will use it, because we still don't have the RDS instance and we don't know which one we'll use
 data "aws_iam_policy_document" "rds" {
   statement {
     effect = "Allow"
