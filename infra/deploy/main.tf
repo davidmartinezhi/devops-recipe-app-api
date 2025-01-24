0e65e713f4943e5b7e142722ca8dad5278649fba
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2025-01-23 21:29:55 -0600

    Removed tf state and slash after the key name

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket               = "devops-recipe-app-tf-state-david-demo" # name of the bucket 
-    key                  = "tf-state-deploy/terraform.tfstate"
+    key                  = "tf-state-deploy"
     workspace_key_prefix = "tf-state-deploy-env"
     region               = "us-east-2"
     encrypt              = true
