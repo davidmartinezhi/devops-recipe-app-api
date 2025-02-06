c11d47876c08b084afae31488f66f9a4b8d00499
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2025-02-05 22:46:33 -0600

    Added IAM permissions for ECS

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -253,6 +253,10 @@ resource "aws_iam_user_policy_attachment" "ecs" {
   policy_arn = aws_iam_policy.ecs.arn
 }
 
+resource "aws_iam_service_linked_role" "ecs" {
+  aws_service_name = "ecs.amazonaws.com"
+}
+
 #########################
 # Policy for IAM access #
 #########################
